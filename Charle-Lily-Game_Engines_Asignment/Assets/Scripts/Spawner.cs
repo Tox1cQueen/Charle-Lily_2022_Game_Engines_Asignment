da62ed8ef7f945c572654127ad529f1b7208dd28
Tox1cQueen <77941882+Tox1cQueen@users.noreply.github.com>
2022-12-09 23:25:27 +0000

    Spawning of objects with force
    objects spawn with a force but do not have it in a radom direction yet

--- Charle-Lily-Game_Engines_Asignment/Assets/Scripts/Spawner.cs
+++ Charle-Lily-Game_Engines_Asignment/Assets/Scripts/Spawner.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public Rigidbody prefab;
+    public float randRotate;
+    public float randForce;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        randForce = Random.Range(400f, 500f);
+        randRotate = Random.Range(-180f, 180f);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        Rigidbody spawn;
+        spawn = Instantiate(prefab, transform.up, transform.rotation) as Rigidbody;
+        spawn.AddForce(transform.up * randForce);
     }
 }
+
+

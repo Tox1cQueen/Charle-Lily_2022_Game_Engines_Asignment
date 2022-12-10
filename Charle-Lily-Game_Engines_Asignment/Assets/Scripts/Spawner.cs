260272c59274797610ed10c4efcd86b4cbb25fe0
Tox1cQueen <77941882+Tox1cQueen@users.noreply.github.com>
2022-12-10 14:18:49 +0000

    Fixed the spawning with force
    now has a random rotation for where the object spawns

--- Charle-Lily-Game_Engines_Asignment/Assets/Scripts/Spawner.cs
+++ Charle-Lily-Game_Engines_Asignment/Assets/Scripts/Spawner.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Rigidbody prefab;
-    public float randRotate;
+    public Vector3 randRotate;
     public float randForce;
+    public Vector3 pos;
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        pos = new Vector3(0,0,0);
     }
 
     // Update is called once per frame
     void Update()
     {
         randForce = Random.Range(400f, 500f);
-        randRotate = Random.Range(-180f, 180f);
+        randRotate = new Vector3(Random.Range(-180f, 180f), Random.Range(-180f, 180f), Random.Range(-180f, 180f));
 
         if (Input.GetKeyDown(KeyCode.E))
         {
@@ -29,8 +30,8 @@ public class Spawner : MonoBehaviour
     void Spawn()
     {
         Rigidbody spawn;
-        spawn = Instantiate(prefab, transform.up, transform.rotation) as Rigidbody;
-        spawn.AddForce(transform.up * randForce);
+        spawn = Instantiate(prefab, pos, Random.rotation) as Rigidbody;
+        spawn.AddForce(pos * randForce);
     }
 }
 
